5230e942656bab958277a3daae4e5d3a9dd3cd42
Joshua Velayo <143049573+JoshuaVelayo@users.noreply.github.com>
2026-06-23 09:39:35 -0600

    Added load_dkist_slits, _fits_pixel conversions, and get_hmi_map

--- visp_alignment.py
+++ visp_alignment.py
@@ -7,6 +7,7 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 from sunpy.net import Fido, attrs as a
 import sunpy.map
+import sunpy.data.sample
 import dkist.net
 import dkist
 
@@ -16,10 +17,17 @@ class Config:
     Specifies all settings, paths, and other metaparameters used in the alignment
     """
 
-    def __init__(self, path_to_dkist_data: str, path_to_sunpy: str, verbose: bool):
+    def __init__(
+        self,
+        path_to_dkist_data: str,
+        path_to_sunpy: str,
+        wavelength_index: int,
+        verbose: bool,
+    ):
         self.path_to_dkist_data = path_to_dkist_data
         self.path_to_sunpy = path_to_sunpy
         self.verbose = verbose
+        self.wavelength_index = wavelength_index
 
 
 class Data:
@@ -31,9 +39,6 @@ class Data:
         self.cfg = cfg
 
 
-# ------------------------------------------------------------------------------------
-
-
 class DataLoader:
     """
     Loads the DKIST and HMI data, extracts neccessary info from headers or shapes, saves it to be used later
@@ -41,6 +46,84 @@ class DataLoader:
 
     def __init__(self, cfg: Config):
         self.cfg = cfg
+
+    def load_dkist_slits(self):
+        data_dir = Path(self.cfg.path_to_dkist_data)
+
+        all_fits = sorted(data_dir.glob("*.fits"))
+
+        if not all_fits:
+            raise FileNotFoundError(f"No .fits files found in {data_dir}")
+
+        raster = []
+        spatial_x_coordinates = []
+        spatial_y_coordinates = []
+
+        header = fits.getheader(all_fits[0], ext=1)
+
+        nx = header["DNAXIS3"]
+
+        for i in range(nx):
+            path = all_fits[i]
+            data = fits.getdata(path, ext=1)
+            header = fits.getheader(path, ext=1)
+
+            spatial_x, spatial_y = self._fits_pixel_to_spatial(
+                data, header, self.cfg.wavelength_index
+            )
+
+            raster.append(data[0, :, :])
+            spatial_x_coordinates.append(spatial_x)
+            spatial_y_coordinates.append(spatial_y)
+
+        return np.array(raster), spatial_x_coordinates, spatial_y_coordinates
+    
+    def _fits_pixel_to_wavelength(self, data, header):
+        cdelt2 = header["CDELT2"]
+        crpix2 = header["CRPIX2"]
+        crval2 = header["CRVAL2"]
+
+        slice = data[0, :, 0]
+
+        for i in range(len(slice)):
+            wavelength = crval2 + cdelt2 * (i - crpix2)
+            slice[i] = wavelength
+        
+        return slice
+    
+    def _fits_pixel_to_spatial(self, data, header, wavelength_index):
+        cdelt3 = header["CDELT3"]
+        cdelt1 = header["CDELT1"]
+        crpix1 = header["CRPIX1"]
+        crpix3 = header["CRPIX3"]
+        crval1 = header["CRVAL1"]
+        crval3 = header["CRVAL3"]
+        pc1_1 = header["PC1_1"]
+        pc1_3 = header["PC1_3"]
+        pc3_1 = header["PC3_1"]
+        pc3_3 = header["PC3_3"]
+
+        slit = data[0, wavelength_index, :]
+
+        x_pixel = 0
+        spatial_x = []
+        spatial_y = []
+
+        for i in range(slit.size):
+            if not np.isnan(slit[i]):
+                y_pixel = i
+
+                x = crval1 + cdelt1 * (
+                    pc1_1 * (x_pixel - crpix1) + pc1_3 * (y_pixel - crpix3)
+                )
+                y = crval3 + cdelt3 * (
+                    pc3_1 * (x_pixel - crpix1) + pc3_3 * (y_pixel - crpix3)
+                )
+
+                spatial_x.append(x)
+                spatial_y.append(y)
+
+        return spatial_x, spatial_y
 
     def load_hmi(self, start_time: Time, end_time: Time) -> tuple[list, Time]:
         """
@@ -78,6 +161,14 @@ class DataLoader:
 
         return closest_file_path, closest_index, closest_time, time_offset
 
+    def get_hmi_map(self, index: int, file_paths: list):
+        """
+        Returns the map from the path at the passed index
+        """
+        map = sunpy.map.Map(file_paths[index])
+
+        return map
+
 
 class Interpolator:
     """
@@ -95,3 +186,29 @@ class Alignment:
 
     def __init__(self, cfg: Config):
         self.cfg = cfg
+
+
+cfg = Config(
+    "/Users/joshua/projects/nso/dkist-data/pid_2_31/JPUAIO", "~/sunpy/data", 30, True
+)
+
+loader = DataLoader(cfg)
+
+raster, x, y = loader.load_dkist_slits()
+
+print(raster.shape)
+
+wave_index = 30  
+spatial_map = raster[:, wave_index, :]
+
+plt.figure(figsize=(12, 4))
+
+img = plt.imshow(spatial_map, aspect='equal', cmap='magma', origin='lower')
+
+plt.colorbar(img, label='Intensity')
+plt.xlabel('Spatial Y Axis (2556 channels / Arcseconds)')
+plt.ylabel('Spatial X Axis (200 channels / Arcseconds)')
+plt.title(f'Monochromatic Spatial Map at Wavelength Index {wave_index}')
+plt.show()
+
+print("done")
